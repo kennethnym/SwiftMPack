88ed5ab90207e9610f5c0e85e13288453c62c198
Kenneth <kennethnym@outlook.com>
2024-08-03 22:36:53 +0100

    feat: impl array decoding

--- Sources/SwiftMPack/Decoder.swift
+++ Sources/SwiftMPack/Decoder.swift
@@ -1,11 +1,11 @@
 import Foundation
 
 class MPDecoder: Decoder {
-    var codingPath: [any CodingKey] = []
-    var userInfo: [CodingUserInfoKey: Any] = [:]
+    let codingPath: [any CodingKey] = []
+    let userInfo: [CodingUserInfoKey: Any] = [:]
     
     fileprivate let reader: MPTreeReader
-    fileprivate var codingNode: [MPTreeReader.Node] = []
+    fileprivate var codingNodes: [MPTreeReader.Node] = []
     
     static func decode<T: Decodable>(_ type: T.Type, from bytes: [UInt8]) throws -> T {
         guard let reader = MPTreeReader(readFrom: bytes) else {
@@ -17,7 +17,7 @@ class MPDecoder: Decoder {
     
     init(startingFrom node: MPTreeReader.Node, readingFrom reader: MPTreeReader) {
         self.reader = reader
-        codingNode.append(node)
+        codingNodes.append(node)
     }
     
     func container<Key>(keyedBy type: Key.Type) throws -> KeyedDecodingContainer<Key> where Key: CodingKey {
@@ -25,7 +25,7 @@ class MPDecoder: Decoder {
     }
     
     func unkeyedContainer() throws -> any UnkeyedDecodingContainer {
-        fatalError()
+        MPUnkeyedDecodingContainer(referencing: self)
     }
     
     func singleValueContainer() throws -> any SingleValueDecodingContainer {
@@ -34,19 +34,19 @@ class MPDecoder: Decoder {
 }
 
 class MPKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContainerProtocol {
-    var codingPath: [any CodingKey] = []
-    var allKeys: [Key] = []
+    let codingPath: [any CodingKey] = []
+    let allKeys: [Key] = []
     
     private let decoder: MPDecoder
     private let currentNode: MPTreeReader.Node
     
     init(referencing decoder: MPDecoder) {
         self.decoder = decoder
-        self.currentNode = decoder.codingNode.last!
+        self.currentNode = decoder.codingNodes.last!
     }
     
     deinit {
-        decoder.codingNode.removeLast()
+        decoder.codingNodes.removeLast()
     }
     
     func contains(_ key: Key) -> Bool {
@@ -118,23 +118,151 @@ class MPKeyedDecodingContainer<Key: CodingKey>: KeyedDecodingContainerProtocol {
     
     func decode<T>(_ type: T.Type, forKey key: Key) throws -> T where T: Decodable {
         let nextNode: MPTreeReader.Node = decoder.reader.read(key.stringValue, in: currentNode)
-        decoder.codingNode.append(nextNode)
+        decoder.codingNodes.append(nextNode)
         return try .init(from: decoder)
     }
     
     func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type, forKey key: Key) throws -> KeyedDecodingContainer<NestedKey> where NestedKey: CodingKey {
-        return KeyedDecodingContainer(MPKeyedDecodingContainer<NestedKey>(referencing: decoder))
+        KeyedDecodingContainer(MPKeyedDecodingContainer<NestedKey>(referencing: decoder))
     }
     
     func nestedUnkeyedContainer(forKey key: Key) throws -> any UnkeyedDecodingContainer {
-        fatalError()
+        MPUnkeyedDecodingContainer(referencing: decoder)
     }
     
-    func superDecoder() throws -> any Decoder {
-        fatalError()
+    func superDecoder() throws -> any Decoder { try superDecoder(forKey: .init(stringValue: "super")!) }
+    
+    func superDecoder(forKey key: Key) throws -> any Decoder { decoder }
+}
+
+class MPUnkeyedDecodingContainer: UnkeyedDecodingContainer {
+    var codingPath: [any CodingKey] = []
+    var isAtEnd: Bool { _count == currentIndex }
+    var count: Int? { _count }
+    
+    private let decoder: MPDecoder
+    private let currentNode: MPTreeReader.Node
+    
+    private var _count: Int
+    private(set) var currentIndex: Int = 0
+    
+    init(referencing decoder: MPDecoder) {
+        self.decoder = decoder
+        self.currentNode = decoder.codingNodes.last!
+        self._count = decoder.reader.readArrayLength(of: currentNode)
     }
     
-    func superDecoder(forKey key: Key) throws -> any Decoder {
-        fatalError()
+    deinit {
+        decoder.codingNodes.removeLast()
     }
+    
+    func decodeNil() throws -> Bool {
+        let isNil = decoder.reader.isArrayItemNil(currentIndex, in: currentNode)
+        currentIndex += 1
+        return isNil
+    }
+    
+    func decode(_ type: String.Type) throws -> String {
+        guard let str: String = decoder.reader.readInArray(currentIndex, in: currentNode) else {
+            throw DecodingError.dataCorruptedError(in: self, debugDescription: "Invalid string at index \(currentIndex)")
+        }
+        currentIndex += 1
+        return str
+    }
+    
+    func decode(_ type: Double.Type) throws -> Double {
+        let value: Double = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Float.Type) throws -> Float {
+        let value: Float = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Int.Type) throws -> Int {
+        let value: Int = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Int8.Type) throws -> Int8 {
+        let value: Int8 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Int16.Type) throws -> Int16 {
+        let value: Int16 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Int32.Type) throws -> Int32 {
+        let value: Int32 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Int64.Type) throws -> Int64 {
+        let value: Int64 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: UInt.Type) throws -> UInt {
+        let value: UInt = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: UInt8.Type) throws -> UInt8 {
+        let value: UInt8 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: UInt16.Type) throws -> UInt16 {
+        let value: UInt16 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: UInt32.Type) throws -> UInt32 {
+        let value: UInt32 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: UInt64.Type) throws -> UInt64 {
+        let value: UInt64 = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+    
+    func decode(_ type: Bool.Type) throws -> Bool {
+        let value: Bool = decoder.reader.readInArray(currentIndex, in: currentNode)
+        currentIndex += 1
+        return value
+    }
+
+    func decode<T>(_ type: T.Type) throws -> T where T: Decodable {
+        let node: MPTreeReader.Node = decoder.reader.readInArray(currentIndex, in: currentNode)
+        decoder.codingNodes.append(node)
+        let value = try type.init(from: decoder)
+        currentIndex += 1
+        return value
+    }
+    
+    func nestedContainer<NestedKey>(keyedBy type: NestedKey.Type) throws -> KeyedDecodingContainer<NestedKey> where NestedKey: CodingKey {
+        .init(MPKeyedDecodingContainer(referencing: decoder))
+    }
+    
+    func nestedUnkeyedContainer() throws -> any UnkeyedDecodingContainer {
+        MPUnkeyedDecodingContainer(referencing: decoder)
+    }
+    
+    func superDecoder() throws -> any Decoder { decoder }
 }
--- Sources/SwiftMPack/TreeReader.swift
+++ Sources/SwiftMPack/TreeReader.swift
@@ -115,4 +115,94 @@ class MPTreeReader {
     func read(_ key: String, in node: Node) -> Node {
         mpack_node_map_cstr(node, key)
     }
+
+    func isArrayItemNil(_ i: Int, in arrayNode: Node) -> Bool {
+        mpack_node_is_nil(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Int {
+        let itemNode = mpack_node_array_at(arrayNode, i)
+        let num: any SignedInteger = switch MemoryLayout<Int>.size {
+        case 8: mpack_node_i8(itemNode)
+        case 16: mpack_node_i16(itemNode)
+        case 32: mpack_node_i32(itemNode)
+        case 64: mpack_node_i64(itemNode)
+        default: fatalError()
+        }
+        return Int(num)
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Int8 {
+        mpack_node_i8(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Int16 {
+        mpack_node_i16(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Int32 {
+        mpack_node_i32(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Int64 {
+        mpack_node_i64(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> UInt {
+        let itemNode = mpack_node_array_at(arrayNode, i)
+        let num: any UnsignedInteger = switch MemoryLayout<UInt>.size {
+        case 8: mpack_node_u8(itemNode)
+        case 16: mpack_node_u16(itemNode)
+        case 32: mpack_node_u32(itemNode)
+        case 64: mpack_node_u64(itemNode)
+        default: fatalError()
+        }
+        return UInt(num)
+    }
+
+    func readArrayLength(of node: Node) -> Int {
+        mpack_node_array_length(node)
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> UInt8 {
+        mpack_node_u8(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> UInt16 {
+        mpack_node_u16(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> UInt32 {
+        mpack_node_u32(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> UInt64 {
+        mpack_node_u64(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Bool {
+        mpack_node_bool(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Float {
+        mpack_node_float(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> Double {
+        mpack_node_double(mpack_node_array_at(arrayNode, i))
+    }
+
+    func readInArray(_ i: Int, in arrayNode: Node) -> String? {
+        let itemNode = mpack_node_array_at(arrayNode, i)
+        guard let ptr = mpack_node_str(itemNode) else {
+            return nil
+        }
+        let len = mpack_node_strlen(itemNode)
+        let data = Data(bytes: ptr, count: len)
+        return String(data: data, encoding: .utf8)
+    }
+    
+    func readInArray(_ i: Int, in arrayNode: Node) -> Node {
+        mpack_node_array_at(arrayNode, i)
+    }
 }
