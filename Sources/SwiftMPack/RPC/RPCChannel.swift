d8effe5c0fde3ce82b3ef1b2a94b96b1bffb1fc0
Kenneth <kennethnym@outlook.com>
2024-08-05 01:02:25 +0100

    feat: implement in memory rpc channel

--- Sources/SwiftMPack/RPC/RPCChannel.swift
+++ Sources/SwiftMPack/RPC/RPCChannel.swift
@@ -3,5 +3,32 @@ import Foundation
 public protocol MPRPCChannel {
     func send(data: Data)
 
-    func onIncomingData(handler: (Data) -> Void)
+    func onIncomingData(handler: @escaping (Data) -> Void)
+}
+
+public class MPRPCInMemoryRPCChannel: MPRPCChannel {
+    private var handler: ((Data) -> Void)?
+    public var otherChannel: MPRPCInMemoryRPCChannel?
+
+    public init() {}
+
+    public init(sendTo channel: MPRPCInMemoryRPCChannel) {
+        otherChannel = channel
+    }
+
+    public func send(data: Data) {
+        otherChannel?.received(data: data)
+    }
+
+    func received(data: Data) {
+        if let handler = handler {
+            DispatchQueue.global(qos: .default).async {
+                handler(data)
+            }
+        }
+    }
+
+    public func onIncomingData(handler: @escaping (Data) -> Void) {
+        self.handler = handler
+    }
 }
